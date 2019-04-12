ca8d9b195b5195477ce62fbe62fa089366e0aacf
Anthony Flores <Anthony.Flores2@iam.local>
2019-04-12 18:15:34 -0500

    Added 2 tutorial levels

---
+++ Assets/Scripts/Events/Tutorial1Start.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial1Start : MonoBehaviour, IEvents
+{
+
+    [SerializeField] MainCharacter character;
+
+    public void TriggerEvent()
+    {
+        character.StopMainCharacter();
+    }
+}
