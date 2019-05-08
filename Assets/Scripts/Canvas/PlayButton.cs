2924dc91483e45adb6aaeb192a34170a1d8d4f87
Anthony Flores <Anthony.Flores2@iam.local>
2019-05-07 21:31:57 -0500

    New Menu

---
+++ Assets/Scripts/Canvas/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string sceneToLoad;
+
+    public void OnOVRTriggerPressed()
+    {
+    SceneManager.LoadScene(sceneToLoad);
+    }
+}
---
+++ Assets/Scripts/Canvas/TutorialLineRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialLineRenderer : MonoBehaviour
+{
+
+    [SerializeField] private Transform PointA;
+    [SerializeField] private Transform PointB;
+
+    private LineRenderer lineDraw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineDraw = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        lineDraw.SetPosition(0, PointA.position);
+        lineDraw.SetPosition(1, PointB.position);
+    }
+}
